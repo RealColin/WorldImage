9363bf663b3c7e8625635e29fefdb5b2e17e2e40
Werner, Colin <crwgond@gmail.com>
2024-12-31 03:03:21 -0500

    Added humidity density function to terrain and fleshed out ocean.json

--- common/src/main/java/realcolin/worldimage/worldgen/terrain/Terrain.java
+++ common/src/main/java/realcolin/worldimage/worldgen/terrain/Terrain.java
@@ -5,19 +5,17 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.RegistryFileCodec;
 import net.minecraft.world.level.levelgen.DensityFunction;
-import net.minecraft.world.level.levelgen.NoiseRouter;
 import realcolin.worldimage.WorldImageRegistries;
 
-import java.util.function.Function;
-
-public record Terrain(DensityFunction continents, DensityFunction erosion, DensityFunction height, DensityFunction ridges, DensityFunction temperature) {
+public record Terrain(DensityFunction continents, DensityFunction erosion, DensityFunction ridges, DensityFunction height, DensityFunction temperature, DensityFunction humidity) {
     public static final Codec<Terrain> DIRECT_CODEC =
             RecordCodecBuilder.create(joe -> joe.group(
                     DensityFunction.DIRECT_CODEC.fieldOf("continents").forGetter(src -> src.continents),
                     DensityFunction.DIRECT_CODEC.fieldOf("erosion").forGetter(src -> src.erosion),
-                    DensityFunction.DIRECT_CODEC.fieldOf("height").forGetter(src -> src.height),
                     DensityFunction.DIRECT_CODEC.fieldOf("ridges").forGetter(src -> src.ridges),
-                    DensityFunction.DIRECT_CODEC.fieldOf("temperature").forGetter(src -> src.temperature)
+                    DensityFunction.DIRECT_CODEC.fieldOf("height").forGetter(src -> src.height),
+                    DensityFunction.DIRECT_CODEC.fieldOf("temperature").forGetter(src -> src.temperature),
+                    DensityFunction.DIRECT_CODEC.fieldOf("humidity").forGetter(src -> src.humidity)
             ).apply(joe, Terrain::new));
 
     public static final Codec<Holder<Terrain>> CODEC = RegistryFileCodec.create(WorldImageRegistries.TERRAIN, DIRECT_CODEC);
